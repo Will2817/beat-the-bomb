c380a02e81f657e0d5be3b7e552507d1d47b2887
CaptOkie <henriknoetze@cmail.carleton.ca>
2017-04-23 19:10:12 -0400

    Fixed test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Text from 'material-ui/Text'
@@ -7,13 +8,15 @@ import './App.css'
 class App extends Component {
   render () {
     return (
-      <div>
-        <AppBar>
-          <Toolbar>
-            <Text type='title' colorInherit>Beat the Bomb</Text>
-          </Toolbar>
-        </AppBar>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <AppBar>
+            <Toolbar>
+              <Text type='title' colorInherit>Beat the Bomb</Text>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import './index.css'
 
-const Root = () => (
-  <MuiThemeProvider>
-    <App />
-  </MuiThemeProvider>
-)
-
 ReactDOM.render(
-  <Root />,
+  <App />,
   document.getElementById('root')
 )
