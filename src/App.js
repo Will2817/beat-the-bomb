082b928228ce7608c52d10cb150f579795cfc0d7
William Clifford <willclifford2817@gmail.com>
2017-04-23 18:45:30 -0400

    Fixed lint issues

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Text from 'material-ui/Text';
-import './App.css';
+import React, { Component } from 'react'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Text from 'material-ui/Text'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div>
         <AppBar>
           <Toolbar>
-            <Text type="title" colorInherit>Beat the Bomb</Text>
+            <Text type='title' colorInherit>Beat the Bomb</Text>
           </Toolbar>
         </AppBar>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import './index.css'
 
 const Root = () => (
   <MuiThemeProvider>
     <App />
   </MuiThemeProvider>
-);
+)
 
 ReactDOM.render(
   <Root />,
   document.getElementById('root')
-);
+)
