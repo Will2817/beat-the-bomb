1bc371347f3a117174de2a75c76466591dcd84aa
William Clifford <willclifford2817@gmail.com>
2017-04-24 21:05:51 -0400

    New UI attempt 2

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react'
 import { Layout } from 'antd'
 import './App.css'
-import SiderBar from '../SideBar'
+import SideBar from '../SideBar'
 // import BombInfo from './BombInfo'
 
 const { Header, Sider, Content } = Layout
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      collapsed: false,
+      mode: 'inline'
+    }
+  }
+  onCollapse (collapsed) {
+    this.setState({
+      collapsed,
+      mode: collapsed ? 'vertical' : 'inline'
+    })
+  }
   render () {
     return (
       <Layout className='root-layout'>
-        <Header>Header</Header>
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={(collapsed) => this.onCollapse(collapsed)} >
+          <SideBar mode={this.state.mode} />
+        </Sider>
         <Layout>
-          <Sider><SiderBar /></Sider>
+          <Header>Bomb Info Page</Header>
           <Content>Content</Content>
         </Layout>
       </Layout>
--- src/SideBar/index.js
+++ src/SideBar/index.js
@@ -1,50 +1,31 @@
 import React, { Component } from 'react'
 import { Menu, Icon } from 'antd'
-const SubMenu = Menu.SubMenu
+import './SideBar.css'
+
+const ItemGroup = Menu.ItemGroup
 
 class SideBar extends Component {
-  constructor () {
-    super()
-    this.state = {
-      current: '1'
-    }
-  }
   handleClick (e) {
-    console.log('click ', e)
-    this.setState({
-      current: e.key
-    })
+
   }
   render () {
     return (
       <div>
+        <div className='side-bar-logo'>B<span className='hidden-logo-text'>eat The Bomb</span></div>
         <Menu
+          className='side-bar-menu'
           theme='dark'
           onClick={this.handleClick}
-          defaultOpenKeys={['sub1']}
-          selectedKeys={[this.state.current]}
-          mode='inline'
+          mode={this.props.mode}
         >
-          <SubMenu key='sub1' title={<span><Icon type='mail' /><span>Navigation One</span></span>}>
+          <ItemGroup key='sub1' title={<span><Icon type='rocket' /><span className='nav-text'>Active Modules</span></span>}>
             <Menu.Item key='1'>Option 1</Menu.Item>
             <Menu.Item key='2'>Option 2</Menu.Item>
-            <Menu.Item key='3'>Option 3</Menu.Item>
-            <Menu.Item key='4'>Option 4</Menu.Item>
-          </SubMenu>
-          <SubMenu key='sub2' title={<span><Icon type='appstore' /><span>Navigtion Two</span></span>}>
-            <Menu.Item key='5'>Option 5</Menu.Item>
-            <Menu.Item key='6'>Option 6</Menu.Item>
-            <SubMenu key='sub3' title='Submenu'>
-              <Menu.Item key='7'>Option 7</Menu.Item>
-              <Menu.Item key='8'>Option 8</Menu.Item>
-            </SubMenu>
-          </SubMenu>
-          <SubMenu key='sub4' title={<span><Icon type='setting' /><span>Navigation Three</span></span>}>
-            <Menu.Item key='9'>Option 9</Menu.Item>
-            <Menu.Item key='10'>Option 10</Menu.Item>
-            <Menu.Item key='11'>Option 11</Menu.Item>
-            <Menu.Item key='12'>Option 12</Menu.Item>
-          </SubMenu>
+          </ItemGroup>
+          <ItemGroup key='sub2' title={<span><Icon type='file-add' /><span className='nav-text'>All Module</span></span>}>
+            <Menu.Item key='5'>Option 5<Icon type='plus' /></Menu.Item>
+            <Menu.Item key='6'>Option 6<Icon type='plus' /></Menu.Item>
+          </ItemGroup>
         </Menu>
       </div>
     )
