02a7d53801401cd7bbc91c99c352629e899adf33
William Clifford <willclifford2817@gmail.com>
2017-04-29 15:04:08 -0400

    Can delete modules now

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Layout } from 'antd'
+import { Layout, Button, Row, Col } from 'antd'
 import './App.css'
 import SideBar from '../SideBar'
 import BombInfo from '../BombInfo'
@@ -37,17 +37,28 @@ class App extends Component {
     modules.push({id: uuid(), type})
     this.setState({modules})
   }
+  removeModule (index) {
+    var modules = this.state.modules.slice()
+    modules.splice(index, 1)
+    this.setState({modules})
+  }
   render () {
     const modules = this.state.modules.map((module, index) => {
       switch (module.type) {
         case 'Password': {
-          return <div id={index}><PasswordModule key={module.id} /></div>
+          return (<div id={index} key={module.id}>
+            <div style={{'display': 'flex', 'flex-direction': 'row'}}>
+              <h2 style={{'display': 'flex', 'flex': '1'}}>Password</h2><Button type='danger' icon='delete' onClick={() => this.removeModule(index)} />
+            </div>
+            <PasswordModule />
+          </div>)
         }
         default: return null
       }
     })
 
     return (
+
       <Layout className='root-layout'>
         <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse.bind(this)} >
           <SideBar mode={this.state.mode} modules={this.state.modules} addModule={this.addModule.bind(this)} />
@@ -55,9 +66,13 @@ class App extends Component {
         <Layout>
           <Header><BombInfo bombInfo={this.state.bombInfo} handleFieldChange={this.handleFieldChange.bind(this)} /></Header>
           <Content>
-            <Layout>
-              {modules}
-            </Layout>
+            <Row>
+              <Col span={12}>
+                <Layout>
+                  {modules}
+                </Layout>
+              </Col>
+            </Row>
           </Content>
         </Layout>
       </Layout>
