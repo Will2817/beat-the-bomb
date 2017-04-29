2f8570ef50010c61116df653d9077d11ef8a52d8
William Clifford <willclifford2817@gmail.com>
2017-04-29 17:00:50 -0400

    Fixed BombInfo styling

--- src/BombInfo/index.js
+++ src/BombInfo/index.js
@@ -5,34 +5,37 @@ const FormItem = Form.Item
 
 class BombInfo extends Component {
   render () {
-    const formItemLayout = {
-      labelCol: { span: 14 },
-      wrapperCol: { span: 14 }
+    const colLayout = {
+      xs: 8,
+      sm: 4
     }
     return (
       <div>
-        <Row>
-          <Col span={4} ><FormItem {...formItemLayout} label={'# of Batteries'}>
-            <InputNumber value={this.props.bombInfo.numBatteries}
-              onChange={(value) => this.props.handleFieldChange('numBatteries', value)} />
-          </FormItem></Col>
-          <Col span={4} ><FormItem {...formItemLayout} label={'Serial #'}>
-            <Input value={this.props.bombInfo.serialNum}
-              onChange={(value) => this.props.handleFieldChange('serialNum', value)} />
-          </FormItem></Col>
-          <Col span={4} ><FormItem {...formItemLayout} label={'# of Strikes'}>
-            <InputNumber value={this.props.bombInfo.numStrikes}
-              onChange={(value) => this.props.handleFieldChange('numStrikes', value)} />
-          </FormItem></Col>
+        <Row className='header-row'>
+          <Col className='header-col' {...colLayout}>
+            <Row><label># of Batteries</label></Row>
+            <Row><InputNumber value={this.props.bombInfo.numBatteries}
+              onChange={(value) => this.props.handleFieldChange('numBatteries', value)} /></Row>
+          </Col>
+          <Col className='header-col' {...colLayout}>
+            <Row><label>Serial #</label></Row>
+            <Row><Input value={this.props.bombInfo.serialNum}
+              onChange={(value) => this.props.handleFieldChange('serialNum', value)} /></Row>
+          </Col>
+          <Col className='header-col' {...colLayout}>
+            <Row><label># of Strikes</label></Row>
+            <Row><InputNumber value={this.props.bombInfo.numStrikes}
+              onChange={(value) => this.props.handleFieldChange('numStrikes', value)} /></Row>
+          </Col>
         </Row>
-        <Row>
-          <Col span={4} ><Checkbox value={this.props.bombInfo.carLabel}
+        <Row className='header-row'>
+          <Col className='header-col' {...colLayout}><Checkbox value={this.props.bombInfo.carLabel}
             onChange={(value) => this.props.handleFieldChange('carLabel', value)} >Lit CAR Label</Checkbox>
           </Col>
-          <Col span={4} ><Checkbox value={this.props.bombInfo.frkLabel}
+          <Col className='header-col' {...colLayout}><Checkbox value={this.props.bombInfo.frkLabel}
             onChange={(value) => this.props.handleFieldChange('frkLabel', value)}>Lit FRK Label</Checkbox>
           </Col>
-          <Col span={4} ><Checkbox value={this.props.bombInfo.parallelPort}
+          <Col className='header-col' {...colLayout}><Checkbox value={this.props.bombInfo.parallelPort}
             onChange={(value) => this.props.handleFieldChange('parallelPort', value)} >Parrallel Port</Checkbox>
           </Col>
         </Row>
