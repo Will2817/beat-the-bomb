4b8965dcd53b15b138c9610c828204de6fa7c8d5
William Clifford <willclifford2817@gmail.com>
2017-04-29 13:56:23 -0400

    Fixed sider styling

--- src/SideBar/index.js
+++ src/SideBar/index.js
@@ -18,28 +18,31 @@ class SideBar extends Component {
     return (
       <div>
         <div className='side-bar-logo'>B<span className='hidden-logo-text'>eat The Bomb</span></div>
-        <Menu
-          className='side-bar-menu'
-          theme='dark'
-          mode={this.props.mode}
-          selectable={false}
-        >
-          <ItemGroup key='sub1' title={<span><Icon type='rocket' /><span className='nav-text'>Active Modules</span></span>}>
-            {menuItems}
-          </ItemGroup>
-        </Menu>
-        <Menu
-          className='side-bar-menu'
-          theme='dark'
-          onClick={(e) => this.props.addModule(e.key)}
-          mode={this.props.mode}
-          selectable={false}
-        >
-          <ItemGroup key='sub2' title={<span><Icon type='file-add' /><span className='nav-text'>All Modules</span></span>}>
-            <Menu.Item key='Password'><Icon type='lock' />Password<Icon type='plus' /></Menu.Item>
+        <div>
+          <Menu
+            className='side-bar-menu'
+            theme='dark'
+            onClick={(e) => this.props.addModule(e.key)}
+            mode={this.props.mode}
+            selectable={false}
+          >
+            <ItemGroup key='sub2' title={<span><Icon type='file-add' /><span className='nav-text'>All Modules</span></span>}>
+              <Menu.Item key='Password'><Icon type='lock' />Password<Icon type='plus' /></Menu.Item>
 
-          </ItemGroup>
-        </Menu>
+            </ItemGroup>
+          </Menu>
+          <Menu
+            className='side-bar-menu'
+            theme='dark'
+            mode={this.props.mode}
+            selectable={false}
+          >
+            <ItemGroup key='sub1' title={<span><Icon type='rocket' /><span className='nav-text'>Active Modules</span></span>}>
+              {menuItems}
+            </ItemGroup>
+          </Menu>
+
+        </div>
       </div>
     )
   }
