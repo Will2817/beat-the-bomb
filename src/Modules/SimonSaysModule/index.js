8b109bd4c18085847be7db81d5ff6565338be6ab
William Clifford <willclifford2817@gmail.com>
2017-04-29 23:54:52 -0400

    Added simon says

---
+++ src/Modules/SimonSaysModule/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { Table, Alert } from 'antd'
+import './button.css'
+
+class SimonSaysModule extends Component {
+  vowelInSerial () {
+    return /[aeiou]/i.test(this.props.bombInfo.serialNum)
+  }
+  render () {
+    var element = (<Alert message='Missing "Serial #" field' type='error' />)
+    if (this.props.bombInfo.serialNum) {
+      var dataSource
+      if (this.vowelInSerial()) {
+        switch (this.props.bombInfo.numStrikes || 0) {
+          case 0: dataSource = [{key: '1', r: 'Blue', b: 'Red', g: 'Yellow', y: 'Green'}]
+            break
+          case 1:dataSource = [{key: '1', r: 'Yellow', b: 'Green', g: 'Blue', y: 'Red'}]
+            break
+          case 2: dataSource = [{key: '1', r: 'Green', b: 'Red', g: 'Yellow', y: 'Blue'}]
+            break
+        }
+      } else {
+        switch (this.props.bombInfo.numStrikes || 0) {
+          case 0: dataSource = [{key: '1', r: 'Blue', b: 'Yellow', g: 'Green', y: 'Red'}]
+            break
+          case 1: dataSource = [{key: '1', r: 'Red', b: 'Blue', g: 'Yellow', y: 'Green'}]
+            break
+          case 2: dataSource = [{key: '1', r: 'Yellow', b: 'Green', g: 'Blue', y: 'Red'}]
+            break
+        }
+      }
+      element = (
+        <Table size='small' pagination={false}
+          columns={[{title: 'Red', dataIndex: 'r'}, {title: 'Blue', dataIndex: 'b'}, {title: 'Green', dataIndex: 'g'}, {title: 'Yellow', dataIndex: 'y'}]}
+          dataSource={dataSource}
+        />)
+    }
+    return (
+      <div className='simon-says-module'>
+        <Alert message='This module will require the following fields: "Serial #" and "Strikes"' type='info' />
+        {element}
+      </div>
+    )
+  }
+}
+
+export const element = SimonSaysModule
+export const heading = 'Simon Says'
+export const icon = 'notification'
+export const state = { }
--- src/Modules/index.js
+++ src/Modules/index.js
@@ -1,6 +1,7 @@
 export default [
   require('./WireModule'),
   require('./ButtonModule'),
+  require('./SimonSaysModule'),
   require('./PasswordModule'),
   require('./KnobModule')
 ]
